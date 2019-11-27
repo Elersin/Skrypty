a7b962e7fbcbed3e4074caf0651c644f646d660b
Elersin <Avolonir@wp.pl>
2019-11-27 23:33:18 +0100

    Add new files

---
+++ Zegar.js
@@ -0,0 +1,21 @@
+function funkcja() {
+
+var dzisiaj = new Date();
+
+var dzien = dzisiaj.getDate();
+var miesiac = dzisiaj.getMonth()+1;
+var rok = dzisiaj.getFullYear();
+var godzina = dzisiaj.getHours();
+var minuta = dzisiaj.getMinutes();
+   if (minuta<10) { minuta = "0" + minuta;}
+var sekunda = dzisiaj.getSeconds();
+   if (sekunda<10) { sekunda = "0" + sekunda;}
+   
+  
+   document.getElementById('zegar').innerHTML = dzien + "." + miesiac + "." + rok + " | " + godzina + "." + minuta + "." + sekunda;
+   
+   setTimeout("funkcja()",1000);
+
+   }
+
+
